760839c34923fd493d9fef241c00b673a91d996e
Zack <zackmeadowspro@gmail.com>
2018-03-14 02:02:14 -0400

    InitialProduct
    Includes a landing page and the foundation for excel sheet upload & processing

---
+++ Capstone/Capstone/Scripts/site/filemanager.js
@@ -0,0 +1,9 @@
+﻿
+// Manages upload button name change & generation button availability
+$(document).on('change', '#schedule-upload :file', function () {
+    var input = $(this), label = input.val().replace(/\\/g, '/').replace(/.*\//, '');
+    if (input.val()) {
+        $('#schedule-generate').css("display", "block");
+        $('#schedule-upload .btn-label').text(label);
+    }
+});
