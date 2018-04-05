dd16aa572d41b6f2377aae26030540c62f839d32
Brian-Paquette <b.p.paquette@hotmail.com>
2018-04-05 10:59:43 -0400

    added get for calendar
    and some other stuff

--- Capstone/Capstone/Controllers/HomeController.cs
+++ Capstone/Capstone/Controllers/HomeController.cs
@@ -7,11 +7,12 @@ using Microsoft.Owin.Security;
 using Microsoft.Owin.Security.Cookies;
 using Microsoft.Owin.Security.OpenIdConnect;
 using System.Configuration;
-using System.Net.Http.Headers;
 using System.Threading.Tasks;
 using System.Security.Claims;
 using Microsoft.Identity.Client;
 using Capstone.TokenStorage;
+using Microsoft.Graph;
+using System.Net.Http.Headers;
 
 namespace Capstone.Controllers
 {
@@ -19,7 +20,101 @@ namespace Capstone.Controllers
     {
         public ActionResult Index()
         {
+            if (Request.IsAuthenticated)
+            {
+                
+                string userId = ClaimsPrincipal.Current.FindFirst(ClaimTypes.NameIdentifier).Value;
+                string userName = ClaimsPrincipal.Current.FindFirst(ClaimTypes.Email).Value;
+                if (string.IsNullOrEmpty(userId))
+                {
+                    // Invalid principal, sign out
+                    return RedirectToAction("SignOut");
+                }
+
+                // Since we cache tokens in the session, if the server restarts
+                // but the browser still has a cached cookie, we may be
+                // authenticated but not have a valid token cache. Check for this
+                // and force signout.
+                SessionTokenCache tokenCache = new SessionTokenCache(userId, HttpContext);
+                if (!tokenCache.HasData())
+                {
+                    // Cache is empty, sign out
+                    return RedirectToAction("SignOut");
+                }
+
+                ViewBag.UserName = userName;
+            }
             return View();
+        }
+
+        public async Task<ActionResult> Inbox()
+        {
+            string token = await GetAccessToken();
+            if (string.IsNullOrEmpty(token))
+            {
+                // If there's no token in the session, redirect to Home
+                return Redirect("/");
+            }
+
+            GraphServiceClient client = new GraphServiceClient(
+                new DelegateAuthenticationProvider(
+                    (requestMessage) =>
+                    {
+                        requestMessage.Headers.Authorization =
+                            new AuthenticationHeaderValue("Bearer", token);
+
+                        return Task.FromResult(0);
+                    }));
+
+            try
+            {
+                var mailResults = await client.Me.MailFolders.Inbox.Messages.Request()
+                                    .OrderBy("receivedDateTime DESC")
+                                    .Select("subject,receivedDateTime,from")
+                                    .Top(10)
+                                    .GetAsync();
+
+                return View(mailResults.CurrentPage);
+            }
+            catch (ServiceException ex)
+            {
+                return RedirectToAction("Error", "Home", new { message = "ERROR retrieving messages", debug = ex.Message });
+            }
+        }
+
+        public async Task<ActionResult> Calendar()
+        {
+            string token = await GetAccessToken();
+            if (string.IsNullOrEmpty(token))
+            {
+                // If there's no token in the session, redirect to Home
+                return Redirect("/");
+            }
+
+            GraphServiceClient client = new GraphServiceClient(
+                new DelegateAuthenticationProvider(
+                    (requestMessage) =>
+                    {
+                        requestMessage.Headers.Authorization =
+                            new AuthenticationHeaderValue("Bearer", token);
+
+                        return Task.FromResult(0);
+                    }));
+
+            try
+            {
+                var eventResults = await client.Me.Events.Request()
+                                    .OrderBy("start/dateTime DESC")
+                                    .Select("subject,start,end")
+                                    .Top(10)
+                                    .GetAsync();
+
+                return View(eventResults.CurrentPage);
+            }
+            catch (ServiceException ex)
+            {
+                return RedirectToAction("Error", "Home", new { message = "ERROR retrieving events", debug = ex.Message });
+            }
         }
 
         public ActionResult About()
@@ -35,17 +130,7 @@ namespace Capstone.Controllers
 
             return View();
         }
-        public async Task<ActionResult> Inbox()
-        {
-            string token = await GetAccessToken();
-            if (string.IsNullOrEmpty(token))
-            {
-                // If there's no token in the session, redirect to Home
-                return Redirect("/");
-            }
-
-            return Content(string.Format("Token: {0}", token));
-        }
+        
         public void SignIn()
         {
             if (!Request.IsAuthenticated)
@@ -58,9 +143,21 @@ namespace Capstone.Controllers
 
         public void SignOut()
         {
+            if (Request.IsAuthenticated)
+            {
+                string userId = ClaimsPrincipal.Current.FindFirst(ClaimTypes.NameIdentifier).Value;
+
+                if (!string.IsNullOrEmpty(userId))
+                {
+                    // Get the user's token cache and clear it
+                    SessionTokenCache tokenCache = new SessionTokenCache(userId, HttpContext);
+                    tokenCache.Clear();
+                }
+            }
+            // Send an OpenID Connect sign-out request. 
             HttpContext.GetOwinContext().Authentication.SignOut(
-                    OpenIdConnectAuthenticationDefaults.AuthenticationType,
-                    CookieAuthenticationDefaults.AuthenticationType);
+                CookieAuthenticationDefaults.AuthenticationType);
+            Response.Redirect("/");
         }
         public async Task<string> GetAccessToken()
         {
