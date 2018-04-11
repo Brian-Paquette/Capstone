5efd1551aed626776cc975476c0e904df9184ff5
Zack <zackmeadowspro@gmail.com>
2018-04-11 19:09:25 -0400

    Auth Protection for other pages

--- Capstone/Capstone/Controllers/HomeController.cs
+++ Capstone/Capstone/Controllers/HomeController.cs
@@ -22,7 +22,6 @@ namespace Capstone.Controllers
         {
             if (Request.IsAuthenticated)
             {
-                
                 string userId = ClaimsPrincipal.Current.FindFirst(ClaimTypes.NameIdentifier).Value;
                 string userName = ClaimsPrincipal.Current.FindFirst(ClaimTypes.Email).Value;
                 if (string.IsNullOrEmpty(userId))
@@ -49,72 +48,80 @@ namespace Capstone.Controllers
 
         public async Task<ActionResult> Inbox()
         {
-            string token = await GetAccessToken();
-            if (string.IsNullOrEmpty(token))
+            if (Request.IsAuthenticated)
             {
-                // If there's no token in the session, redirect to Home
-                return Redirect("/");
+                string token = await GetAccessToken();
+                if (string.IsNullOrEmpty(token))
+                {
+                    // If there's no token in the session, redirect to Home
+                    return Redirect("/");
+                }
+
+                GraphServiceClient client = new GraphServiceClient(
+                    new DelegateAuthenticationProvider(
+                        (requestMessage) =>
+                        {
+                            requestMessage.Headers.Authorization =
+                                new AuthenticationHeaderValue("Bearer", token);
+
+                            return Task.FromResult(0);
+                        }));
+
+                try
+                {
+                    var mailResults = await client.Me.MailFolders.Inbox.Messages.Request()
+                                        .OrderBy("receivedDateTime DESC")
+                                        .Select("subject,receivedDateTime,from")
+                                        .Top(10)
+                                        .GetAsync();
+
+                    return View(mailResults.CurrentPage);
+                }
+                catch (ServiceException ex)
+                {
+                    return RedirectToAction("Error", "Home", new { message = "ERROR retrieving messages", debug = ex.Message });
+                }
             }
-
-            GraphServiceClient client = new GraphServiceClient(
-                new DelegateAuthenticationProvider(
-                    (requestMessage) =>
-                    {
-                        requestMessage.Headers.Authorization =
-                            new AuthenticationHeaderValue("Bearer", token);
-
-                        return Task.FromResult(0);
-                    }));
-
-            try
-            {
-                var mailResults = await client.Me.MailFolders.Inbox.Messages.Request()
-                                    .OrderBy("receivedDateTime DESC")
-                                    .Select("subject,receivedDateTime,from")
-                                    .Top(10)
-                                    .GetAsync();
-
-                return View(mailResults.CurrentPage);
-            }
-            catch (ServiceException ex)
-            {
-                return RedirectToAction("Error", "Home", new { message = "ERROR retrieving messages", debug = ex.Message });
-            }
+            else { return RedirectToAction("SignOut", "Home", null); }
         }
 
         public async Task<ActionResult> Calendar()
         {
-            string token = await GetAccessToken();
-            if (string.IsNullOrEmpty(token))
+            if (Request.IsAuthenticated)
             {
-                // If there's no token in the session, redirect to Home
-                return Redirect("/");
+                string token = await GetAccessToken();
+                if (string.IsNullOrEmpty(token))
+                {
+                    // If there's no token in the session, redirect to Home
+                    return Redirect("/");
+                }
+
+                GraphServiceClient client = new GraphServiceClient(
+                    new DelegateAuthenticationProvider(
+                        (requestMessage) =>
+                        {
+                            requestMessage.Headers.Authorization =
+                                new AuthenticationHeaderValue("Bearer", token);
+
+                            return Task.FromResult(0);
+                        }));
+
+                try
+                {
+                    var eventResults = await client.Me.Events.Request()
+                                        .OrderBy("start/dateTime DESC")
+                                        .Select("subject,start,end")
+                                        .Top(10)
+                                        .GetAsync();
+
+                    return View(eventResults.CurrentPage);
+                }
+                catch (ServiceException ex)
+                {
+                    return RedirectToAction("Error", "Home", new { message = "ERROR retrieving events", debug = ex.Message });
+                }
             }
-
-            GraphServiceClient client = new GraphServiceClient(
-                new DelegateAuthenticationProvider(
-                    (requestMessage) =>
-                    {
-                        requestMessage.Headers.Authorization =
-                            new AuthenticationHeaderValue("Bearer", token);
-
-                        return Task.FromResult(0);
-                    }));
-
-            try
-            {
-                var eventResults = await client.Me.Events.Request()
-                                    .OrderBy("start/dateTime DESC")
-                                    .Select("subject,start,end")
-                                    .Top(10)
-                                    .GetAsync();
-
-                return View(eventResults.CurrentPage);
-            }
-            catch (ServiceException ex)
-            {
-                return RedirectToAction("Error", "Home", new { message = "ERROR retrieving events", debug = ex.Message });
-            }
+            else { return RedirectToAction("SignOut", "Home", null); }
         }
         
         public void SignIn()
